ac20716331d0bdcdae22461f73dd486adb91da12
Majorjohn2023 <majorjohn983@gmail.com>
2024-03-15 19:39:34 +0530

    Update model file aasdasdfadsfszsdfssdfssdds

--- models/model_332.sql
+++ models/model_332.sql
@@ -1,5 +1,3 @@
 {{ config(materialized='table') }}
 
-SELECT m.*, t.*
-FROM {{ ref('model_331') }} m
-JOIN snowflake_sample_data.tpch_sf1.orders t ON m.l_orderkey = t.o_orderkey
+SELECT * FROM {{ ref('model_331') }}
--- models/model_555.sql
+++ models/model_555.sql
@@ -1,5 +1,3 @@
 {{ config(materialized='table') }}
 
-SELECT m.*, t.*
-FROM {{ ref('model_554') }} m
-JOIN snowflake_sample_data.tpch_sf1.supplier t ON m.l_suppkey = t.s_suppkey
+SELECT * FROM {{ ref('model_554') }}
